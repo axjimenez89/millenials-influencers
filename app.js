b1d88dd791bd648afb39d49a07633fc4b357ecc2
Alejandra Jimenez <axjimenez89@gmail.com>
2017-07-27 21:54:46 -0400

    adding code

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express')
-var app = express()
+var app = module.exports = express();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -8,3 +13,25 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+//connecting mysql for sessions 
+var options = {
+    host: 'localhost',
+    port: 3306,
+  
+};
+ 
+var sessionStore = new MySQLStore(options);
+ 
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+}));
+
+var connection = mysql.createConnection(options); // or mysql.createPool(options); 
+var sessionStore = new MySQLStore({}/* session store options */, connection);
+
+
